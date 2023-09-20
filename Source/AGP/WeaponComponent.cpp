798c884c395ace93526c48f12f4070156a90295d
Samuel McKenzie-Sell <samuel.m.mckenzie-sell@student.uts.edu.au>
2023-09-20 14:27:05 +1000

    Lab7 F - WeaponComponent Class

---
+++ Source/AGP/WeaponComponent.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeaponComponent.h"
+
+
+// Sets default values for this component's properties
+UWeaponComponent::UWeaponComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+bool UWeaponComponent::Fire(const FVector& BulletStart, const FVector& FireAtLocation)
+{
+	return false;
+}
+
+
+// Called when the game starts
+void UWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/AGP/WeaponComponent.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WeaponComponent.generated.h"
+
+
+UENUM(BlueprintType) // Allows us to use this enum in blueprints.
+enum class EWeaponType : uint8 {
+	Rifle,
+	Pistol
+};
+
+USTRUCT(BlueprintType)
+struct FWeaponStats
+{
+	 GENERATED_BODY()
+public:
+	 
+	 EWeaponType WeaponType = EWeaponType::Rifle;
+	 float Accuracy = 1.0f;
+	 float FireRate = 0.2f;
+	 float BaseDamage = 10.0f;
+	 int32 MagazineSize = 5;
+};
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AGP_API UWeaponComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UWeaponComponent();
+
+	bool Fire(const FVector& BulletStart, const FVector& FireAtLocation);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	FWeaponStats WeaponStats;
+	int32 RoundsRemainingInMagazine;
+	float TimeSinceLastShot = 0.0f;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
